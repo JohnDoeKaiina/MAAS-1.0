dcd289de8d72b071889fc49021bd79a8da720995
Tobi Awolaju <120692959+tobi-awolaju-21@users.noreply.github.com>
2023-09-10 14:00:27 +0100

    Update model_01_app.py

--- model_01_app.py
+++ model_01_app.py
@@ -15,22 +15,14 @@ model = tf.keras.models.load_model('./models/sequential_0910_0309')
 def classify_image(image):
     img = Image.open(BytesIO(image))
     img = img.resize((IMG_WIDTH, IMG_HEIGHT))
+    image = np.zeros((150, 150,3))
     img = np.array(img)
     img = np.expand_dims(img, axis=0)
     shape = img.shape
     st.write("The shape of your input is:", shape)
 
-    
-    image = np.zeros((150, 150,3))
-    # Add a new dimension at axis=2
-    expanded_image = np.expand_dims(image, axis=0)
-    shape2 = expanded_image.shape
-    st.write("expanded image is:", shape2)
 
-
-
-
-    classes = model.predict(expanded_image, batch_size=10)
+    classes = model.predict(img, batch_size=10)
     return classes[0][0]
 
 def main():
