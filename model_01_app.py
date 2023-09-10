1c2f7cdb77bb9fad9fab5cf975c999b1d88cd188
Tobi Awolaju <120692959+tobi-awolaju-21@users.noreply.github.com>
2023-09-10 05:46:02 +0100

    Update model_01_app.py

--- model_01_app.py
+++ model_01_app.py
@@ -20,9 +20,9 @@ def predict_class(image, model):
     #img = np.expand_dims(img, axis=0)
     
 	#image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [180, 180])
+	#image = tf.image.resize(image, [180, 180])
+	
 	image = np.expand_dims(image, axis = 0)
-
 	prediction = model.predict(image)
 
 	return prediction
