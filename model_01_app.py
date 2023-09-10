bb95eb079b2c5033750dd0d77644cec11e3e6d5f
Tobi Awolaju <120692959+tobi-awolaju-21@users.noreply.github.com>
2023-09-10 14:13:14 +0100

    Update model_01_app.py

--- model_01_app.py
+++ model_01_app.py
@@ -21,7 +21,7 @@ def classify_image(image):
     st.write("The shape of your input is:", shape)
 
     
-    image = np.zeros((150, 151,3))
+    image = np.zeros((150, 150,3))
     # Add a new dimension at axis=2
     expanded_image = np.expand_dims(image, axis=0)
     shape2 = expanded_image.shape
@@ -30,7 +30,7 @@ def classify_image(image):
 
 
 
-    classes = model.predict(expanded_image, batch_size=10)
+    classes = model.predict(img, batch_size=10)
     return classes[0][0]
 
 def main():
