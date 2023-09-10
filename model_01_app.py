6e4363f2c276e94ad4c4de5ac3950d899da4ad03
Tobi Awolaju <120692959+tobi-awolaju-21@users.noreply.github.com>
2023-09-10 05:31:45 +0100

    Update model_01_app.py

--- model_01_app.py
+++ model_01_app.py
@@ -49,8 +49,8 @@ else:
 
 	class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
-	result = class_names[np.argmax(pred)]
-
+	result = np.argmax(pred)
+	
 	output = 'The image is a ' + result
 
 	slot.text('Done')
